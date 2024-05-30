a523353645c1d9c646ab8e610787d7549e360bde
Saucelo <jacobsaucelo32@gmail.com>
2024-05-30 13:02:58 +0800

    migrating to react

--- cube.js
+++ cube.js
@@ -8,14 +8,22 @@ import {
 } from "three/examples/jsm/Addons.js";
 
 const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000
+// );
 const camera = new THREE.PerspectiveCamera(
-  75,
+  70,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("canvas"),
+});
 renderer.shadowMap.enabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
