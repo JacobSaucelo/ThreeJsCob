9eaa153186d96fbda284dc576d89ceda96c21ec6
Saucelo <jacobsaucelo32@gmail.com>
2024-05-30 11:22:15 +0800

    PIXEEEEEEELLLL RENDER WORKING

--- cube.js
+++ cube.js
@@ -1,5 +1,11 @@
 import * as THREE from "three";
 import WebGL from "three/addons/capabilities/WebGL.js";
+import {
+  EffectComposer,
+  OrbitControls,
+  OutputPass,
+  RenderPixelatedPass,
+} from "three/examples/jsm/Addons.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -10,8 +16,19 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const renderer = new THREE.WebGLRenderer();
+renderer.shadowMap.enabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const composer = new EffectComposer(renderer);
+const renderPixelatedPass = new RenderPixelatedPass(6, scene, camera);
+composer.addPass(renderPixelatedPass);
+
+const outputPass = new OutputPass();
+composer.addPass(outputPass);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.maxZoom = 2;
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -23,10 +40,8 @@ camera.position.z = 5;
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
   renderer.render(scene, camera);
+  composer.render();
 }
 
 if (WebGL.isWebGLAvailable()) {
